31953e42346212a2b3cceaefa996f77374a0cb7b
edgara1006 <34774244+edgara1006@users.noreply.github.com>
2018-02-13 23:37:35 -0500

    Add files via upload

---
+++ student_aide_tdd/spec/vendindMachine.js
@@ -0,0 +1,60 @@
+var vending_machine = function(arr,item,num){
+  // code here
+     if(!Array.isArray(arr) || !arr.length>0 || !arr.every(theArr=> typeof theArr === "array")){
+        throw new TypeError;
+    }
+
+    
+    var theArr = ["Pepsi","Coke","Sprite"];
+    for(var i = 0; i<arr.length;i++){
+        if(theArr !== "Harry Potter"){
+            output.push("Gryffindor");
+        }else if(person === "Voldemort"){
+            output.push("Slytherin");
+        }else{
+            var house = houses[Math.floor(Math.random()*houses.length)]; //random house
+            output.push(house);
+        }
+    }
+  
+
+    return theArr;
+}
+
+////////////////////
+//// Unit Tests ////
+////////////////////
+
+describe("vending_machine", function(){
+  it("should be a function", function(){
+    expect(vending_machine instanceof Function).toBeTruthy();
+  });
+
+  it("should throw an Error if the first argument is not an array", function(){
+    expect(()=>vending_machine("Not an array","Pepsi",1)).toThrow();
+  });
+
+  it("should throw an Error if the second argument is not a string", function(){
+    expect(()=>vending_machine("Pepsi",1,1)).toThrow();
+  });
+  
+  it("should throw an Error if the third argument is not an number", function(){
+    expect(()=>vending_machine("Pepsi","Pepsi","Not a Number")).toThrow();
+  });
+
+  it("should return the same array given as an argument", function(){
+    var theArr = ["Pepsi","Coke","Sprite"];
+    expect(vending_machine(theArr,"Coke", 2)).toBe(theArr);
+  });
+
+  it("should return the array with the item added to it the number of times given", function(){
+    var theArr = ["Pepsi","Sprite"];
+    expect(vending_machine(theArr,"Coke", 1)).toContain("Coke");
+    expect(vending_machine(theArr,"Coke", 3).filter(x=>x==="Coke").length).toBe(4); // 4 since should have added from previous assertion
+  });
+
+  it("should be able to handle an empty array for input", function(){
+    var theArr = [];
+    expect(()=>vending_machine(theArr,"Coke", 2)).not.toThrow();
+  });
+});
